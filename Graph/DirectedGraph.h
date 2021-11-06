52318a59bae2764abf9fcb1ef1c9eb326ff755b7
DiegoGCh <64449084+DiegoGCh@users.noreply.github.com>
2021-11-06 17:20:06 -0500

    Updated DirectedGraph

--- Graph/DirectedGraph.h
+++ Graph/DirectedGraph.h
@@ -6,6 +6,23 @@
 template<typename TV, typename TE>
 class DirectedGraph : public Graph<TV, TE>{
 
+    public:
+        DirectedGraph() = default;
+
+        ~DirectedGraph();
+
+        int numEdges = 0;
+
+        DirectedGraph(string id, TV vertex){
+            this->insertVertex(id, vertex);
+        };
+
+        bool findById(string id){
+            if (this->vertexes.find(id) == this->vertexes.end()){
+                return false;
+            } return true;
+        };
+
 };
 
 #endif
