fafd1039dfe8ff506e4a7fd45c20f10abb81336d
Jer_MC <63933660+JerM1609@users.noreply.github.com>
2021-11-18 23:28:28 -0500

    Add files via upload

--- Tester/tester.h
+++ Tester/tester.h
@@ -61,7 +61,7 @@ namespace Tester
 	    cout << "3. Prim\n";
 	    cout << "================================================" << endl;
 	    //cin >> input;
-	    input = 2;
+	    input = 4;
 	    switch (input)
 	    {
 	    	case 1:
@@ -100,12 +100,12 @@ namespace Tester
 	    		prim.apply();
 	    		break;
 	    	}
-		case 4:
+	    	case 4:
 	    	{
 	    		fname = "Tester/connected_dir.txt";
 	    		FEngine.insertElementsInGraph(fname, graph);
 	    		cout << boolalpha << graph->isConnected() << endl;
-	    		//cout << boolalpha << graph->isStronglyConnected() << endl;
+	    		cout << boolalpha << graph->isStronglyConnected() << endl;
 	    	}
 	    	default: break;
 	    }
@@ -125,7 +125,7 @@ namespace Tester
 	    cout << "2. Directed\n";
 	    cout << "================================================" << endl;
 	    //cin >> typeG;
-	    typeG = 1;
+	    typeG = 2;
 	    if (typeG == 1)
 	    	runAlgorithms(ugraph);
     	else 
